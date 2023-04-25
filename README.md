cc5b131a19266f0a97d824a6a8ab74b04e7ae331
JayLiao <f64016130@gmail.com>
2023-04-25 18:23:44 +0800

    fix: remove html.tpl at the end fix: update README.md

--- README.md
+++ README.md
@@ -8,6 +8,11 @@ https://aquasecurity.github.io/trivy/v0.40/getting-started/installation/
 brew install trivy
 ```
 
+# 將腳本設定為可執行
+```
+chmod +x trivy.sh
+``` 
+
 # 掃描腳本
 ./trivy.sh <專案資料夾路徑>
 
--- trivy.sh
+++ trivy.sh
@@ -3,3 +3,4 @@
 docker build -t oring/edge-ems-deploy-client:develop $1
 curl -LJO https://raw.githubusercontent.com/aquasecurity/trivy/main/contrib/html.tpl
 trivy image --format template --template "@html.tpl" -o $1/report.html oring/edge-ems-deploy-client:develop
+rm -f html.tpl
