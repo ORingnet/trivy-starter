a4b4b26cdcb242cd7b6ab3a9cab088ea2e337a1c
JayLiao <f64016130@gmail.com>
2023-04-26 16:27:08 +0800

    fix: prune images after image building fix: change the way to obtain and use the Trivy template file

--- trivy.sh
+++ trivy.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 docker build -t oring/edge-ems-deploy-client:develop $1
-curl -LJO https://raw.githubusercontent.com/aquasecurity/trivy/main/contrib/html.tpl
-trivy image --format template --template "@html.tpl" -o $1/report.html oring/edge-ems-deploy-client:develop
-rm -f html.tpl
+docker image prune -f
+trivy image --format template --template "`curl -sL https://raw.githubusercontent.com/aquasecurity/trivy/main/contrib/html.tpl`" -o $1/report.html oring/edge-ems-deploy-client:develop
